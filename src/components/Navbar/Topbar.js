0b9c43480896e2bee890798b0720a020e67f8d2f
abdur rouf <abdurroufkpkh.gmail.com>
2025-07-21 11:14:41 +0600

    language cut

--- src/components/Navbar/Topbar.js
+++ src/components/Navbar/Topbar.js
@@ -4,10 +4,10 @@ import { GoLocation } from "react-icons/go";
 const Topbar = () => {
   return (
     <div className="w-full bg-sky-200 border-b border-gray-200 text-gray-700 text-xs sm:text-sm md:text-base">
-      <div className="max-w-7xl mx-auto px-4 py-2 flex justify-between items-center">
+      <div className="w-full mx-auto px-4 py-2 flex justify-between items-center">
         
         {/* Left Side: Location & Phone */}
-        <div className="flex items-center gap-4 sm:gap-8 whitespace-nowrap">
+        <div className="flex items-center  gap-1 sm:gap-6 whitespace-nowrap">
           
           <div className="flex items-center gap-1">
             <FaPhoneAlt className="text-gray-600" />
@@ -19,7 +19,7 @@ const Topbar = () => {
           </div>
         </div>
 
-        {/* Right Side: Language Icon */}
+        {/* Right Side: Language Icon
         <div className="flex items-center gap-1 cursor-pointer">
           <FaGlobe className="text-gray-700" />
           <select className="border px-2 py-1 text-xs rounded bg-white">
@@ -27,7 +27,7 @@ const Topbar = () => {
               
             </select>
         </div>
-        
+         */}
       </div>
     </div>
   );
